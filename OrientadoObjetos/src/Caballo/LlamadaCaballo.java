193419c72140788c35c9ea4511cd2ab115db4b23
ik013043z1 <ik013043z1@hz341928-206-03.H013043.NET>
2018-12-05 09:47:01 +0100

    LLamada caballo

---
+++ OrientadoObjetos/src/Caballo/LlamadaCaballo.java
@@ -0,0 +1,21 @@
+package Caballo;
+
+public class LlamadaCaballo {
+
+	public static void main(String[] args) {
+	    
+	    Caballo a = new Caballo("Axer", "marrón moteado", 6, 24);
+	    Caballo l = new Caballo("Lykos", "negro", 8, 61);
+	    
+	    System.out.println("Hola, me llamo " + a.getNombre());
+	    a.cabalga();
+	    a.relincha();
+	      
+	    System.out.println("Hola, yo soy " + l.getNombre());
+	    l.rumia();
+	    l.cabalga();
+	  }
+	}
+
+
+
